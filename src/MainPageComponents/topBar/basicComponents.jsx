46fdc221a5d94cbdb7d99d79421c004ce9840f28
Karthik Vignesh Sundar <karthik.vignesh.sun@gmail.com>
2024-01-05 00:29:44 +0000

    created and added filter components in the filter floating window

---
+++ src/MainPageComponents/topBar/basicComponents.jsx
@@ -0,0 +1,131 @@
+import PropTypes from 'prop-types';
+import '../../index.css';
+import { useImmer } from 'use-immer';
+
+function H6Heading({additionalClasses, headingText}){
+
+    return (
+        <h6 
+            className={"text-2xl font-semibold text-primary-cl " + additionalClasses}
+        >
+            {headingText}
+        </h6>
+    );
+}
+
+H6Heading.propTypes = {
+    additionalClasses: PropTypes.string,
+    headingText: PropTypes.string,
+};
+
+function LabelText({additionalClasses, labelText}){
+
+    return (
+        <label 
+            className={"text-sm font-semibold text-primary-cl " + additionalClasses}
+        >
+            {labelText}
+        </label>
+    );
+}
+
+LabelText.propTypes = {
+    additionalClasses: PropTypes.string,
+    labelText: PropTypes.string,
+};
+
+function CheckboxInputField({additionalClasses, fieldName, handleInputChange}){ 
+    
+        const [checkedStatus, setCheckedStatus] = useImmer(false);
+
+        const handleCheckedChange = () => {
+            setCheckedStatus(!checkedStatus);
+            handleInputChange(fieldName);
+        }
+
+        return (
+            <input 
+                type="checkbox"
+                className={"w-4 h-4 rounded-md border border-primary-cl " + additionalClasses}
+                checked={checkedStatus}
+                onChange={handleCheckedChange}
+            />
+        );
+}
+
+CheckboxInputField.propTypes = {
+    additionalClasses: PropTypes.string,
+    fieldName: PropTypes.string,
+    handleInputChange: PropTypes.func,
+};
+
+function NumberInputField({additionalClasses, fieldName, handleInputChange}){
+
+    return (
+        <input 
+            type="number"
+            className={"w-full h-8 rounded-md border border-primary-cl " + additionalClasses}
+            onChange={(event) => handleInputChange(fieldName, event.target.value)}
+        />
+    );
+}
+
+NumberInputField.propTypes = {
+    additionalClasses: PropTypes.string,
+    fieldName: PropTypes.string,
+    handleInputChange: PropTypes.func,
+};
+
+function DateInputField({additionalClasses, fieldName, handleInputChange}){
+
+    return (
+        <input 
+            type="datetime-local"
+            className={"w-full h-8 rounded-md border border-primary-cl " + additionalClasses}
+            onChange={(event) => handleInputChange(fieldName, event.target.value)}
+            readOnly={false}
+        />
+    );
+}
+
+DateInputField.propTypes = {
+    additionalClasses: PropTypes.string,
+    fieldName: PropTypes.string,
+    handleInputChange: PropTypes.func,
+};
+
+function SelectInputField({additionalClasses, fieldName, handleInputChange, selectOptions}){
+
+    return (
+        <select 
+            className={"w-full h-8 rounded-md border border-primary-cl " + additionalClasses}
+            onChange={(event) => handleInputChange(fieldName, event.target.value)}
+        >
+            {selectOptions.map((option) => (
+                <option 
+                    key={option}
+                    value={option}
+                >
+                    {option}
+                </option>
+            ))}
+        </select>
+    );
+}
+
+SelectInputField.propTypes = {
+    additionalClasses: PropTypes.string,
+    fieldName: PropTypes.string,
+    handleInputChange: PropTypes.func,
+    selectOptions: PropTypes.array,
+};
+
+
+export {
+        H6Heading,
+        LabelText,
+        CheckboxInputField,
+        NumberInputField,
+        DateInputField,
+        SelectInputField,
+        };
--- src/MainPageComponents/topBar/filterMenuComponents.jsx
+++ src/MainPageComponents/topBar/filterMenuComponents.jsx
@@ -1,7 +1,210 @@
 import PropTypes from 'prop-types';
+import React from 'react';
 import '../../index.css';
+import { useImmer } from 'use-immer';
+import {
+        H6Heading,
+        LabelText,
+        CheckboxInputField,
+        NumberInputField,
+        SelectInputField,
+        DateInputField,
+        //DateInputField,
+        } from './basicComponents.jsx';
+
+function FilterParamHeadingSection({ additionalClasses, fieldName, headingText, handleInputChange}){
+    return (
+        <section
+            className={"flex flex-row flex-wrap justify-start " + additionalClasses}
+        >
+            <CheckboxInputField
+                additionalClasses=""
+                fieldName={fieldName}
+                handleInputChange={handleInputChange}
+            />
+            <H6Heading
+                additionalClasses=""
+                headingText={headingText}
+            />
+        </section>
+    );
+}
+
+FilterParamHeadingSection.propTypes = {
+    additionalClasses: PropTypes.string,
+    fieldName: PropTypes.string,
+    headingText: PropTypes.string,
+    handleInputChange: PropTypes.func,
+};
+
+function FilterParamInputSection({ additionalClasses, labelText, children}){
+    return (
+        <section
+            className={"flex flex-row flex-wrap justify-start " + additionalClasses}
+        >
+            <LabelText
+                additionalClasses=""
+                labelText={labelText}
+            />
+            {children}
+        </section>
+    );
+}
+
+FilterParamInputSection.propTypes = {
+    additionalClasses: PropTypes.string,
+    labelText: PropTypes.string,
+    children: PropTypes.node,
+};
+
+function FilterParamSection({additionalClasses, headingText, toggleDisplay, children}){
+
+    return (
+        <div
+            className={"flex flex-row flex-wrap justify-start " + additionalClasses}
+        >
+            <FilterParamHeadingSection
+                additionalClasses=""
+                fieldName={headingText}
+                headingText={headingText}
+                handleInputChange={toggleDisplay}
+            />
+            {children}
+        </div>
+    );
+}
+
+FilterParamSection.propTypes = {
+    additionalClasses: PropTypes.string,
+    headingText: PropTypes.string,
+    toggleDisplay: PropTypes.func,
+    children: PropTypes.node,
+};
+
+function FilterRangeInputParam({ labelText, headingText, fieldName, toggleDisplay, filterParamsVisibility, children}){
+
+    const childrenArray = React.Children.toArray(children);
+    const firstChild = childrenArray[0];
+    const secondChild = childrenArray[1];
+    
+    return (
+        <FilterParamSection
+                additionalClasses=""
+                headingText={headingText}
+                toggleDisplay={() => toggleDisplay(fieldName)}
+            >
+                <FilterParamInputSection
+                    additionalClasses={filterParamsVisibility[fieldName] ? "flex" : "hidden"}
+                    labelText={labelText["rangeStart"]}
+                >
+                    {firstChild}
+                </FilterParamInputSection>
+                <FilterParamInputSection
+                    additionalClasses={filterParamsVisibility[fieldName] ? "flex" : "hidden"}
+                    labelText={labelText["rangeStart"]}
+                >
+                    {secondChild}
+                </FilterParamInputSection>
+            </FilterParamSection>
+    );
+}
+
+FilterRangeInputParam.propTypes = {
+    labelText: PropTypes.object,
+    headingText: PropTypes.string,
+    fieldName: PropTypes.string,
+    toggleDisplay: PropTypes.func,
+    filterParamsVisibility: PropTypes.object,
+    children: PropTypes.node,
+};
+
+function FilterSelectInputParam({ headingText, fieldName, toggleDisplay, handleInputChange, filterParamsVisibility, selectOptions}){
+    return (
+        <FilterParamSection
+                additionalClasses=""
+                headingText={headingText}
+                toggleDisplay={() => toggleDisplay(fieldName)}
+            >
+                <FilterParamInputSection
+                    additionalClasses={filterParamsVisibility[fieldName] ? "flex" : "hidden"}
+                    labelText=""
+                >
+                    <SelectInputField
+                        additionalClasses=""
+                        fieldName={fieldName}
+                        handleInputChange={handleInputChange}
+                        selectOptions={selectOptions}
+                    />
+                </FilterParamInputSection>
+        </FilterParamSection>
+    );
+}
+
+FilterSelectInputParam.propTypes = {
+    headingText: PropTypes.string,
+    fieldName: PropTypes.string,
+    toggleDisplay: PropTypes.func,
+    handleInputChange: PropTypes.func,
+    filterParamsVisibility: PropTypes.object,
+    selectOptions: PropTypes.array,
+};
+
+function FilterSortInputParam({ toggleDisplay, handleInputChange, filterParamsVisibility}){
+    
+    const fields = ["value", 
+                    "currency", 
+                    "transactionType", 
+                    "transactionCategory", 
+                    "fromType", 
+                    "fromEntity", 
+                    "toType", 
+                    "toEntity", 
+                    "recurringEntity", 
+                    "createdDate", 
+                    "modifiedDate", 
+                    "transactionDate",
+                ]
+    
+    return (
+        <FilterParamSection
+            additionalClasses=""
+            headingText="Sort"
+            toggleDisplay={() => toggleDisplay("sort")}
+        >
+            <FilterParamInputSection
+                additionalClasses={filterParamsVisibility["sort"] ? "flex" : "hidden"}
+                labelText="Ascending Order"
+            >
+                <SelectInputField
+                    additionalClasses=""
+                    fieldName="sortAscending"
+                    handleInputChange={handleInputChange}
+                    selectOptions={["true", "false"]}
+                />
+            </FilterParamInputSection>
+            <FilterParamInputSection
+                additionalClasses={filterParamsVisibility["sort"] ? "flex" : "hidden"}
+                labelText="Select Field"
+            >
+                <SelectInputField
+                    additionalClasses=""
+                    fieldName="sortField"
+                    handleInputChange={handleInputChange}
+                    selectOptions={fields}
+                />
+            </FilterParamInputSection>
+        </FilterParamSection>
+    );
+}
+
+FilterSortInputParam.propTypes = {
+    toggleDisplay: PropTypes.func,
+    handleInputChange: PropTypes.func,
+    filterParamsVisibility: PropTypes.object,
+};
 
 function FilterMenu({ displayState="hidden", handleFilterClick }){
+    
 
     //each filter param is enabled or disabled based on the whether
     //they are checked in the checkbox or not
@@ -32,12 +235,187 @@ function FilterMenu({ displayState="hidden", handleFilterClick }){
                                   },
                         };
 
+
+    const [filterParamsVisibility, setFilterParamsVisibility] = useImmer({
+                                                                            value: false,
+                                                                            currency: false,
+                                                                            transactionType: false,
+                                                                            transactionCategory: false,
+                                                                            fromType: false,
+                                                                            fromEntity: false,
+                                                                            toType: false,
+                                                                            toEntity: false,
+                                                                            recurringEntity: false,
+                                                                            createdDate: false,
+                                                                            modifiedDate: false,
+                                                                            transactionDate: false,
+                                                                            sort: false,
+                                                                        });
+
+    const toggleDisplay = (fieldName) => {
+        setFilterParamsVisibility(draft => {
+            draft[fieldName] = !draft[fieldName];
+        });
+    }
+
+    const handleInputChange = (fieldName, fieldValue) => {
+        console.log(fieldName, fieldValue);
+    };
+
+    //get from backend api
+    const currencies = ["RON", "EUR", "USD"];
+    const transactionCategories = ["Salary", "Food", "Clothes", "Rent", "Utilities", "Other"];
+    const transactionEntities = [{name: "entity1", type: "Internal"}, {name: "entity2", type: "Internal"}, {name: "entity3", type: "External"}, {name: "entity4", type: "External"}];
+    const recurringEntities = ["entity1", "entity2", "entity3", "entity4"];
+    //convert transactionEntities to an array of transactionEntity names
+    const transactionEntityNames = Object.keys(transactionEntities).map((key) => transactionEntities[key].name );
+
     return (
         <div 
-            className={"h-96 w-[50%] z-50 top-[65px] left-[30%] absolute rounded-lg border bg-surface-cl drop-shadow-lg " + displayState}
+            className={"h-96 w-[50%] z-50 top-[65px] left-[30%] absolute rounded-lg border bg-surface-cl drop-shadow-lg overflow-x-hidden overflow-y-scroll " + displayState}
             onClick={(event) => handleFilterClick(event, filterParams)}
         >
-            
+            <FilterRangeInputParam
+                labelText={{"rangeStart": "Min", "rangeEnd": "Max"}}
+                headingText="Value"
+                fieldName="value"
+                toggleDisplay={toggleDisplay}
+                filterParamsVisibility={filterParamsVisibility}
+            >
+                <NumberInputField
+                    additionalClasses=""
+                    fieldName="valueMin"
+                    handleInputChange={handleInputChange}
+                />
+                <NumberInputField
+                    additionalClasses=""
+                    fieldName="valueMax"
+                    handleInputChange={handleInputChange}
+                />
+            </FilterRangeInputParam>
+            <FilterSelectInputParam
+                headingText="Currency"
+                fieldName="currency"
+                toggleDisplay={toggleDisplay}
+                handleInputChange={handleInputChange}
+                filterParamsVisibility={filterParamsVisibility}
+                selectOptions={currencies}
+            />
+            <FilterSelectInputParam
+                headingText="Transaction Type"
+                fieldName="transactionType"
+                toggleDisplay={toggleDisplay}
+                handleInputChange={handleInputChange}
+                filterParamsVisibility={filterParamsVisibility}
+                selectOptions={["In", "Out"]}
+            />
+            <FilterSelectInputParam
+                headingText="Transaction Category"
+                fieldName="transactionCategory"
+                toggleDisplay={toggleDisplay}
+                handleInputChange={handleInputChange}
+                filterParamsVisibility={filterParamsVisibility}
+                selectOptions={transactionCategories}
+            />
+            <FilterSelectInputParam
+                headingText="From Type"
+                fieldName="fromType"
+                toggleDisplay={toggleDisplay}
+                handleInputChange={handleInputChange}
+                filterParamsVisibility={filterParamsVisibility}
+                selectOptions={["Internal", "External"]}
+            />
+            <FilterSelectInputParam
+                headingText="From Entity"
+                fieldName="fromEntity"
+                toggleDisplay={toggleDisplay}
+                handleInputChange={handleInputChange}
+                filterParamsVisibility={filterParamsVisibility}
+                selectOptions={transactionEntityNames}
+            />
+            <FilterSelectInputParam
+                headingText="To Type"
+                fieldName="toType"
+                toggleDisplay={toggleDisplay}
+                handleInputChange={handleInputChange}
+                filterParamsVisibility={filterParamsVisibility}
+                selectOptions={["Internal", "External"]}
+            />
+            <FilterSelectInputParam
+                headingText="To Entity"
+                fieldName="toEntity"
+                toggleDisplay={toggleDisplay}
+                handleInputChange={handleInputChange}
+                filterParamsVisibility={filterParamsVisibility}
+                selectOptions={transactionEntityNames}
+            />
+            <FilterSelectInputParam
+                headingText="Recurring Entity"
+                fieldName="recurringEntity"
+                toggleDisplay={toggleDisplay}
+                handleInputChange={handleInputChange}
+                filterParamsVisibility={filterParamsVisibility}
+                selectOptions={recurringEntities}
+            />
+            <FilterRangeInputParam
+                labelText={{"rangeStart": "Min", "rangeEnd": "Max"}}
+                headingText="Created Date"
+                fieldName="createdDate"
+                toggleDisplay={toggleDisplay}
+                filterParamsVisibility={filterParamsVisibility}
+            >
+                <DateInputField
+                    additionalClasses=""
+                    fieldName="createdDateMin"
+                    handleInputChange={handleInputChange}
+                />
+                <DateInputField
+                    additionalClasses=""
+                    fieldName="createdDateMax"
+                    handleInputChange={handleInputChange}
+                />
+            </FilterRangeInputParam>
+            <FilterRangeInputParam
+                labelText={{"rangeStart": "Min", "rangeEnd": "Max"}}
+                headingText="Modified Date"
+                fieldName="modifiedDate"
+                toggleDisplay={toggleDisplay}
+                filterParamsVisibility={filterParamsVisibility}
+            >
+                <DateInputField
+                    additionalClasses=""
+                    fieldName="modifiedDateMin"
+                    handleInputChange={handleInputChange}
+                />
+                <DateInputField
+                    additionalClasses=""
+                    fieldName="modifiedDateMax"
+                    handleInputChange={handleInputChange}
+                />
+            </FilterRangeInputParam>
+            <FilterRangeInputParam
+                labelText={{"rangeStart": "Min", "rangeEnd": "Max"}}
+                headingText="Transaction Date"
+                fieldName="transactionDate"
+                toggleDisplay={toggleDisplay}
+                filterParamsVisibility={filterParamsVisibility}
+            >
+                <DateInputField
+                    additionalClasses=""
+                    fieldName="transactionDateMin"
+                    handleInputChange={handleInputChange}
+                />
+                <DateInputField
+                    additionalClasses=""
+                    fieldName="transactionDateMax"
+                    handleInputChange={handleInputChange}
+                />
+            </FilterRangeInputParam>
+            <FilterSortInputParam
+                toggleDisplay={toggleDisplay}
+                handleInputChange={handleInputChange}
+                filterParamsVisibility={filterParamsVisibility}
+            />
         </div>
     );
 }
