52a8bc5262c761d38fbb65e20760296a8e4902aa
Karthik Vignesh Sundar <s.karthik6789+github_prof@gmail.com>
2024-02-23 03:03:48 +0000

    implemented data value convertion between the front end and the backend and implemented the getItem functions to search with filters

--- electronApp/database/recurringTransactionOperations.cjs
+++ electronApp/database/recurringTransactionOperations.cjs
@@ -23,6 +23,142 @@ function setDB(database) {
     console.log("In setDB: db: " + db);
 }
 
+function convertDataFromDBFormat(data) {
+    data = JSON.parse(JSON.stringify(data));
+    const daysOfTheWeek = new Map(Object.entries({0: "Monday", 1: "Tuesday", 2: "Wednesday", 3: "Thursday", 4: "Friday", 5: "Saturday", 6: "Sunday"}));
+    const monthsOfTheYear = new Map(Object.entries({0: "Jan", 1: "Feb", 2: "Mar", 3: "Apr", 4: "May", 5: "Jun", 6: "Jul", 7: "Aug", 8: "Sep", 9: "Oct", 10: "Nov", 11: "Dec"}));
+
+    const formatDateTime = (dateTime) => {
+        const regex = /^\d{4}[-]\d{2}[-]\d{2}[T]\d{2}[:]\d{2}[:]\d{2}$/;
+        if( regex.test(dateTime) ) {
+            return dateTime.substring(0, 16);
+        }
+        return null;
+    };
+
+    const formatTime = (time) => {
+        const regex = /^\d{2}[:]\d{2}[:]\d{2}$/;
+        if( regex.test(time) ) {
+            return time.substring(0, 5);
+        }
+        return null;
+    };
+
+    const formatNumber = (key, number) => {
+        const regex = /^\d+[.]*\d*$/;
+        if( key === "value" && regex.test(number) ) {
+            return String(number);
+        }
+        return null;
+    };
+
+    const formatDayOfTheWeek = (dayOfTheWeek) => {
+        if (daysOfTheWeek.has(dayOfTheWeek)) {
+            return daysOfTheWeek.get(dayOfTheWeek);
+        }
+        return null;
+    };
+
+    const formatMonthOfTheYear = (monthOfTheYear) => {
+        if (monthsOfTheYear.has(monthOfTheYear)) {
+            return monthsOfTheYear.get(monthOfTheYear);
+        }
+        return null;
+    };
+
+    const recursiveExplorer = function recEx(data) { 
+        Object.keys(data).forEach((key) => { 
+            if (data[key] !== null && typeof data[key] === "object" && !Array.isArray(data[key])) {
+                recEx(data[key]);
+            } else if (data[key]) {
+                let formattedValue = null;
+                //check if it is a date; format it
+                formattedValue = formatDateTime(data[key]);
+                //check if it is a time; format it
+                formattedValue = formattedValue === null ? formatTime(data[key]): formattedValue;
+                //check if it is a number; format it
+                formattedValue = formattedValue === null && key === "value" ? formatNumber(key, data[key]): formattedValue;
+                //check if it is a day of the week; format it
+                formattedValue = formattedValue === null && key === "dayOfTheWeek"? formatDayOfTheWeek(data[key]): formattedValue;
+                //check if it is a month of the year; format it
+                formattedValue = formattedValue === null && key === "month" ? formatMonthOfTheYear(data[key]): formattedValue;
+                data[key] = formattedValue !== null? formattedValue : data[key];
+            }
+        });
+        return data;
+    };
+
+    return recursiveExplorer(data);
+}
+
+function  convertDataToDBFormat(data) {
+    data = JSON.parse(JSON.stringify(data));
+    const daysOfTheWeek = new Map(Object.entries({"Monday": 0, "Tuesday": 1, "Wednesday": 2, "Thursday": 3, "Friday": 4, "Saturday": 5, "Sunday": 6}));
+    const monthsOfTheYear = new Map(Object.entries({"Jan": 0, "Feb": 1, "Mar": 2, "Apr": 3, "May": 4, "Jun": 5, "Jul": 6, "Aug": 7, "Sep": 8, "Oct": 9, "Nov": 10, "Dec": 11}));
+
+    const formatDateTime = (dateTime) => {
+        const regex = /^\d{4}[-]\d{2}[-]\d{2}[T]\d{2}[:]\d{2}$/;
+        if( regex.test(dateTime) ) {
+            return (dateTime + ":00" );
+        }
+        return null;
+    };
+
+    const formatTime = (time) => {
+        const regex = /^\d{2}[:]\d{2}$/;
+        if( regex.test(time) ) {
+            return (time + ":00" );
+        }
+        return null;
+    };
+
+    const formatNumber = (number) => {
+        const regex = /^\d+[.]*\d*$/;
+        if( regex.test(number) ) {
+            return Number(number);
+        }
+        return null;
+    };
+
+    const formatDayOfTheWeek = (dayOfTheWeek) => {
+        if (daysOfTheWeek.has(dayOfTheWeek)) {
+            return daysOfTheWeek.get(dayOfTheWeek);
+        }
+        return null;
+    };
+
+    const formatMonthOfTheYear = (monthOfTheYear) => {
+        if (monthsOfTheYear.has(monthOfTheYear)) {
+            return monthsOfTheYear.get(monthOfTheYear);
+        }
+        return null;
+    };
+
+    const recursiveExplorer = function recEx(data) { 
+        Object.keys(data).forEach((key) => { 
+            if (data[key] !== null && typeof data[key] === "object" && !Array.isArray(data[key])) {
+                recEx(data[key]);
+            } else if (data[key]) {
+                let formattedValue = null;
+                //check if it is a date; format it
+                formattedValue = formatDateTime(data[key]);
+                //check if it is a time; format it
+                formattedValue = formattedValue === null ? formatTime(data[key]): formattedValue;
+                //check if it is a number; format it
+                formattedValue = formattedValue === null && key === "value" ? formatNumber(data[key]): formattedValue;
+                //check if it is a day of the week; format it
+                formattedValue = formattedValue === null ? formatDayOfTheWeek(data[key]): formattedValue;
+                //check if it is a month of the year; format it
+                formattedValue = formattedValue === null ? formatMonthOfTheYear(data[key]): formattedValue;
+                data[key] = formattedValue !== null? formattedValue : data[key];
+            }
+        });
+        return data;
+    };
+
+    return recursiveExplorer(data);
+}
+
 function getAllItems() {
 
     return new Promise((resolve, reject) => {
@@ -78,6 +214,80 @@ function getAllItems() {
 
 function getItems(event, searchParams, filterParamsVisibility) {
     console.log("In getItems: searchParams: ", searchParams, " filterParamsVisibility: ", filterParamsVisibility)
+    searchParams = JSON.parse(JSON.stringify(searchParams));
+    searchParams = convertDataToDBFormat(searchParams);
+    return new Promise((resolve, reject) => { 
+        db.serialize(() => { 
+            const fetchFromReferenceID = new Promise((resolve, reject) => { 
+                db.get(`SELECT id FROM financialEntities WHERE title = ?`, searchParams.fromEntity, (err, row) => { 
+                    if (err) { 
+                        console.log("Recurring Entity: In getItems: fetchFromReferenceID: err: ", err);
+                        reject(true);
+                    } else {
+                        resolve(row && row.length > 0 ? row.id : null);
+                    }
+                });
+            });
+
+            const fetchToReferenceID = new Promise((resolve, reject) => { 
+                db.get(`SELECT id FROM financialEntities WHERE title = ?`, searchParams.toEntity, (err, row) => { 
+                    if (err) { 
+                        console.log("Recurring Entity: In getItems: fetchToReferenceID: err: ", err);
+                        reject(true);
+                    } else {
+                        resolve(row && row.length > 0 ? row.id : null);
+                    }
+                });
+            });
+
+            Promise.all([fetchFromReferenceID, fetchToReferenceID]).then(([fromReferenceID, toReferenceID]) => { 
+                delete searchParams.filter.fromEntity;
+                delete searchParams.filter.toEntity;
+                searchParams.filter.fromReference = fromReferenceID;
+                searchParams.filter.toReference = toReferenceID;
+                let query = `SELECT id, \
+                            title, \
+                            recurringFrequencyType as recurringFrequency, \
+                            value, \
+                            transactionType, \
+                            transactionCategory \
+                            FROM recurringTransactions \
+                            WHERE ((title LIKE "%${searchParams.search}%") OR (description LIKE "%${searchParams.search}%"))`;
+                Object.keys(searchParams.filter).forEach((searchField) => { 
+                    if (searchParams.filter[searchField] === null || !filterParamsVisibility[searchField]) return;
+                    //range based query construction
+                    if (typeof searchParams.filter[searchField] === "object" && searchField !== "sort") {
+                        if (searchParams.filter[searchField].min !== null && searchParams.filter[searchField].max !== null) {
+                            query += ` AND (${searchField} BETWEEN "${searchParams.filter[searchField].min}" AND "${searchParams.filter[searchField].max}")`;
+                        }
+                        return;
+                    }
+                    //value based query construction
+                    if (searchParams.filter[searchField] === null || searchParams.filter[searchField] === "choose" || searchField === "sort") return;
+                    query += ` AND (${searchField} = "${searchParams.filter[searchField]}")`;
+                });
+                //construct query for sort
+                if (searchParams.filter.sort.field !== null && searchParams.filter.sort.field !== "choose") {
+                    query += ` ORDER BY ${searchParams.filter.sort.field} ${searchParams.filter.sort.ascending === "true"? "ASC": searchParams.filter.sort.ascending === "false"? "DESC" : ""}`;
+                 }
+                //query and get the information from the database
+                db.all(query, (err, rows) => {
+                    if (err) {
+                        console.log("Recurring Entity: In getItems: err: ", err);
+                        reject(true);
+                    } else {
+                        resolve(rows? rows : []);
+                    }
+                });
+             }).catch((err) => { 
+                console.log("Recurring Entity: In getItems: err: ", err);
+                reject(true);
+             });
+        });
+    });
+
+    
+    /*
     return [
         { id: 91, 
             title: "name",
@@ -100,7 +310,7 @@ function getItems(event, searchParams, filterParamsVisibility) {
                 transactionType: "Out",
                 transactionCategory: "Groceries",
             }
-    ];
+    ]; */
 }
 
 function createEntry() {
@@ -159,7 +369,7 @@ function createEntry() {
 
 function modifyItem(event, selectedItem) {
     console.log("In modifyItem: selectedItem: ", selectedItem);
-
+    selectedItem = convertDataToDBFormat(selectedItem);
     return new Promise((resolve, reject) => {
         db.serialize(() => {
             const getFromFinancialEntityID = new Promise((resolve, reject) => { 
@@ -343,30 +553,31 @@ function getSelectedItem(event, uuid) {
 
             Promise.all([getFromEntityTitle,
                         getToEntityTitle]).then(([fromFinancialEntityRow, toFinancialEntityRow]) => {
-                resolve({
-                    id: uuid, 
-                    title: recurringTransactionRow.title,
-                    description: recurringTransactionRow.description,
-                    value: recurringTransactionRow.value,
-                    currency: recurringTransactionRow.currency,
-                    transactionType: recurringTransactionRow.transactionType,
-                    transactionCategory: recurringTransactionRow.transactionCategory,
-                    fromType: fromFinancialEntityRow ? fromFinancialEntityRow.type : null,
-                    fromEntity: fromFinancialEntityRow ? fromFinancialEntityRow.title : null,
-                    toType: toFinancialEntityRow ? toFinancialEntityRow.type : null,
-                    toEntity: toFinancialEntityRow ? toFinancialEntityRow.title : null,
-                    createdDate: recurringTransactionRow.createdDate,
-                    modifiedDate: recurringTransactionRow.modifiedDate,
-                    recurringFrequency: {
-                        frequency: recurringTransactionRow.frequency ?? undefined,
-                        dayOfTheWeek: recurringTransactionRow.dayOfTheWeek ?? undefined,
-                        dayOfTheMonth: recurringTransactionRow.dayOfTheMonth ?? undefined,
-                        month: recurringTransactionRow.month ?? undefined,
-                        time: recurringTransactionRow.time?? undefined,
-                    },
-                    recurringTransactionStartDate: recurringTransactionRow.recurringTransactionStartDate,
-                    recurringTransactionEndDate: recurringTransactionRow.recurringTransactionEndDate,
-                });
+                const selectedItem = convertDataFromDBFormat({
+                                                                id: uuid, 
+                                                                title: recurringTransactionRow.title,
+                                                                description: recurringTransactionRow.description,
+                                                                value: recurringTransactionRow.value,
+                                                                currency: recurringTransactionRow.currency,
+                                                                transactionType: recurringTransactionRow.transactionType,
+                                                                transactionCategory: recurringTransactionRow.transactionCategory,
+                                                                fromType: fromFinancialEntityRow ? fromFinancialEntityRow.type : null,
+                                                                fromEntity: fromFinancialEntityRow ? fromFinancialEntityRow.title : null,
+                                                                toType: toFinancialEntityRow ? toFinancialEntityRow.type : null,
+                                                                toEntity: toFinancialEntityRow ? toFinancialEntityRow.title : null,
+                                                                createdDate: recurringTransactionRow.createdDate,
+                                                                modifiedDate: recurringTransactionRow.modifiedDate,
+                                                                recurringFrequency: {
+                                                                    frequency: recurringTransactionRow.frequency ?? undefined,
+                                                                    dayOfTheWeek: recurringTransactionRow.dayOfTheWeek ?? undefined,
+                                                                    dayOfTheMonth: recurringTransactionRow.dayOfTheMonth ?? undefined,
+                                                                    month: recurringTransactionRow.month ?? undefined,
+                                                                    time: recurringTransactionRow.time?? undefined,
+                                                                },
+                                                                recurringTransactionStartDate: recurringTransactionRow.recurringTransactionStartDate,
+                                                                recurringTransactionEndDate: recurringTransactionRow.recurringTransactionEndDate,
+                                                            });
+                resolve(selectedItem);
             });
         }).catch((err) => {
             console.log("Recurring Entity: In getSelectedItem: err: ", err);
