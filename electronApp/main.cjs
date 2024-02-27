4defb9300d63d906d0dbb9a75ef12d8e99922ccc
Karthik Vignesh Sundar <s.karthik6789+github_prof@gmail.com>
2024-02-27 04:31:52 +0000

    set minimum height and width for the window

--- electronApp/main.cjs
+++ electronApp/main.cjs
@@ -29,6 +29,8 @@ function createWindow() {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        minHeight: 600,
+        minWidth: 700,
         webPreferences: {
             nodeIntegration: false,
             contextIsolation: true,
