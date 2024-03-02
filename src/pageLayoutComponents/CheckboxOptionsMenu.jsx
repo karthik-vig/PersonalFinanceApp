8338db9942c768273cc32c4e8092f0761d435d1d
Karthik Vignesh Sundar <s.karthik6789+github_prof@gmail.com>
2024-03-02 09:55:03 +0000

    UI changes to CheckboxOptionsMenu

--- src/pageLayoutComponents/CheckboxOptionsMenu.jsx
+++ src/pageLayoutComponents/CheckboxOptionsMenu.jsx
@@ -23,14 +23,14 @@ function CheckboxOptionsMenu({checkBoxOptions=[],
             className={displayState.state + " absolute z-20 top-0 left-0 flex flex-col flex-nowrap justify-center items-center w-[100%] h-[100%] opacity-100 bg-slate-950/30"}
         >
             <div
-                className="absolute top-[25%] left-[40%] p-2 h-40 w-[30%] flex flex-col flex-nowrap justify-center items-center border border-slate-400 rounded-md bg-slate-100"
+                className="absolute top-[25%] left-[40%] p-2 h-auto w-[30%] flex flex-col flex-nowrap justify-center items-center border border-slate-400 rounded-md bg-slate-100"
             > 
                 <section
                     className="flex flex-row flex-nowrap m-5 p-2 h-10 w-[90%] justify-center items-center"
                 >
                     <FontAwesomeIcon 
                         icon="fa-exclamation-triangle" 
-                        color="#ff0000" 
+                        color="#b34b4b" 
                         className="h-10 w-10"
                     />
                     <p
@@ -39,28 +39,32 @@ function CheckboxOptionsMenu({checkBoxOptions=[],
                         {displayState.message}
                     </p>
                 </section>
-                {checkBoxOptions.map((checkBoxOption) => {
-                    return (
-                        <section 
-                            className="flex flex-row flex-nowrap"
-                            key={checkBoxOption.id}
-                        >
-                            <input type="checkbox" id={checkBoxOption.id} onChange={() => dispatch(setOptions(checkBoxOption.id))}/>
-                            <label htmlFor={checkBoxOption.id}>{checkBoxOption.label}</label>
-                        </section>
-                    );
-                })}
+                <section
+                    className="mb-6 h-auto w-auto flex flex-col flex-nowrap justify-start"
+                >
+                    {checkBoxOptions.map((checkBoxOption) => {
+                        return (
+                            <section 
+                                className="flex flex-row flex-nowrap"
+                                key={checkBoxOption.id}
+                            >
+                                <input type="checkbox" id={checkBoxOption.id} onChange={() => dispatch(setOptions(checkBoxOption.id))}/>
+                                <label htmlFor={checkBoxOption.id}>{checkBoxOption.label}</label>
+                            </section>
+                        );
+                    })}
+                </section>
                 <section
                     className="flex flex-row flex-nowrap"
                 >
                     <button 
-                        className="mx-1 border rounded-md bg-green-600 text-semibold text-white w-20 h-8 hover:bg-green-700 border-green-700"
+                        className="border rounded-md bg-green-600 text-semibold text-white w-20 h-8 hover:bg-green-700 border-green-700"
                         onClick={handleProceed}
                     >
                         Proceed
                     </button>
                     <button 
-                        className="mx-1 border rounded-md bg-red-600 text-semibold text-white w-20 h-8 hover:bg-red-700 border-red-700"
+                        className="ml-14 border rounded-md bg-red-400 text-semibold text-white w-20 h-8 hover:bg-red-700 border-red-700"
                         onClick={() => dispatch(changeDisplayState())}
                     >
                         Cancel
