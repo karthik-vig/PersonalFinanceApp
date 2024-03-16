ad476ec1fbf86b40934bf9ade227acce4f20729a
Karthik Vignesh Sundar <s.karthik6789+github_prof@gmail.com>
2024-03-16 16:12:30 +0000

    implemented more stats for the stat box area in the analysis page.

--- electronApp/database/commonOperations.cjs
+++ electronApp/database/commonOperations.cjs
@@ -5,67 +5,109 @@ function setDB(database) {
   db = database;
 }
 
-function getTotalExpenditure(filterOptions) {
-    return new Promise((resolve, reject) => {
-        
-        const getSumOfTransactionTypeIn = new Promise((resolve, reject) => {
-            let queryStmt = `SELECT SUM(value) as transactionTypeIn \
-                            FROM transactions WHERE transactionType = "In" `;
-            if (filterOptions.transactionCategory !== "All") queryStmt += ` AND (transactionCategory = "${filterOptions.transactionCategory}")`;
-            if (filterOptions.currency !== "All") queryStmt += ` AND (currency = "${filterOptions.currency}")`;
-            if (filterOptions.startDate !== "yyyy-mm-ddThh:mm" && filterOptions.endDate !== "yyyy-mm-ddThh:mm") queryStmt += ` AND (transactionDate BETWEEN "${filterOptions.startDate}" AND "${filterOptions.endDate}")`;
-            db.get(queryStmt, 
-                    (err, row) => { 
-                        if (err) reject(err);
-                        resolve(row.transactionTypeIn);
-                    });
-        });
-        const getSumOfTransactionTypeOut = new Promise((resolve, reject) => {
-            let queryStmt = `SELECT SUM(value) as transactionTypeOut \
-                            FROM transactions WHERE transactionType = "Out" `;
-            if (filterOptions.transactionCategory !== "All") queryStmt += ` AND (transactionCategory = "${filterOptions.transactionCategory}")`;
-            if (filterOptions.currency !== "All") queryStmt += ` AND (currency = "${filterOptions.currency}")`;
-            if (filterOptions.startDate !== "yyyy-mm-ddThh:mm" && filterOptions.endDate !== "yyyy-mm-ddThh:mm") queryStmt += ` AND (transactionDate BETWEEN "${filterOptions.startDate}" AND "${filterOptions.endDate}")`;
-            db.get(queryStmt, 
-                    (err, row) => { 
-                        if (err) reject(err);
-                        resolve(row.transactionTypeOut);
-                    });
-        });
-        Promise.all([getSumOfTransactionTypeIn, getSumOfTransactionTypeOut])
-            .then(([sumOfTransactionTypeIn, sumOfTransactionTypeOut]) => {
-                const expenditure = sumOfTransactionTypeIn - sumOfTransactionTypeOut;
-                resolve(expenditure);
-            }).catch((err) => reject(err));
-    });
-
+function addFilterOptionsToQueryStmt(queryStmt,
+                                     filterOptions,
+                                     selectFilterOptions = {
+                                        transactionType: false,
+                                        transactionCategory: false,
+                                        currency: false,
+                                        date: false,
+                                     }) {
+  if (filterOptions.transactionType !== "All" && filterOptions.transactionType !== "Expenditure" && selectFilterOptions.transactionType) queryStmt += ` AND (transactionType = "${filterOptions.transactionType}")`;
+  if (filterOptions.transactionCategory !== "All" && selectFilterOptions.transactionCategory) queryStmt += ` AND (transactionCategory = "${filterOptions.transactionCategory}")`;
+  if (filterOptions.currency !== "All" && selectFilterOptions.currency) queryStmt += ` AND (currency = "${filterOptions.currency}")`;
+  if (filterOptions.startDate !== "yyyy-mm-ddThh:mm" && filterOptions.endDate !== "yyyy-mm-ddThh:mm" && selectFilterOptions.date) queryStmt += ` AND (transactionDate BETWEEN "${filterOptions.startDate}" AND "${filterOptions.endDate}")`;
+  return queryStmt;
 }
 
+function getSumOfTransactionTypeIn(filterOptions) {
+  return new Promise((resolve, reject) => {
+    let queryStmt = `SELECT SUM(value) as sumOfTransactionTypeIn, \
+                    COUNT(*) as numInTransactions \
+                    FROM transactions WHERE transactionType = "In" `;
+    // if (filterOptions.transactionCategory !== "All") queryStmt += ` AND (transactionCategory = "${filterOptions.transactionCategory}")`;
+    // if (filterOptions.currency !== "All") queryStmt += ` AND (currency = "${filterOptions.currency}")`;
+    // if (filterOptions.startDate !== "yyyy-mm-ddThh:mm" && filterOptions.endDate !== "yyyy-mm-ddThh:mm") queryStmt += ` AND (transactionDate BETWEEN "${filterOptions.startDate}" AND "${filterOptions.endDate}")`;
+    const selectFilterOptions = {
+        transactionType: false,
+        transactionCategory: true,
+        currency: true,
+        date: true,
+    };
+    queryStmt = addFilterOptionsToQueryStmt(queryStmt, filterOptions, selectFilterOptions);
+    db.get(queryStmt, 
+            (err, row) => { 
+                if (err) reject(err);
+                resolve([row.sumOfTransactionTypeIn, row.numInTransactions]);
+            });
+});
+}
+
+function getSumOfTransactionTypeOut(filterOptions) {
+  return new Promise((resolve, reject) => {
+    let queryStmt = `SELECT SUM(value) as sumOfTransactionTypeOut, \
+                     COUNT(*) as numOutTransactions \
+                    FROM transactions WHERE transactionType = "Out" `;
+    // if (filterOptions.transactionCategory !== "All") queryStmt += ` AND (transactionCategory = "${filterOptions.transactionCategory}")`;
+    // if (filterOptions.currency !== "All") queryStmt += ` AND (currency = "${filterOptions.currency}")`;
+    // if (filterOptions.startDate !== "yyyy-mm-ddThh:mm" && filterOptions.endDate !== "yyyy-mm-ddThh:mm") queryStmt += ` AND (transactionDate BETWEEN "${filterOptions.startDate}" AND "${filterOptions.endDate}")`;
+    const selectFilterOptions = {
+      transactionType: false,
+      transactionCategory: true,
+      currency: true,
+      date: true,
+    };
+    queryStmt = addFilterOptionsToQueryStmt(queryStmt, filterOptions, selectFilterOptions);
+    db.get(queryStmt, 
+            (err, row) => { 
+                if (err) reject(err);
+                resolve([row.sumOfTransactionTypeOut, row.numOutTransactions]);
+            });
+  });
+} 
+
+// function getTotalExpenditure(filterOptions) {
+//     return new Promise((resolve, reject) => {
+//         const getSumOfTransactionTypeIn = getSumOfTransactionTypeIn(filterOptions);
+//         const getSumOfTransactionTypeOut = getSumOfTransactionTypeOut(filterOptions);
+//         Promise.all([getSumOfTransactionTypeIn, getSumOfTransactionTypeOut])
+//             .then(([sumOfTransactionTypeIn, sumOfTransactionTypeOut]) => {
+//                 const expenditure = sumOfTransactionTypeIn - sumOfTransactionTypeOut;
+//                 resolve(expenditure);
+//             }).catch((err) => reject(err));
+//     });
+
+// }
+
+
+
 async function getStatsAboutDB(event, filterOptions) {
+        const [sumOfTransactionTypeIn, numInTransactions] = await getSumOfTransactionTypeIn(filterOptions);
+        const [sumOfTransactionTypeOut, numOutTransactions] = await getSumOfTransactionTypeOut(filterOptions);
         const statBoxData = [
             {
               title: "Total Exp.",
-              value: await getTotalExpenditure(filterOptions),
+              value: sumOfTransactionTypeIn - sumOfTransactionTypeOut,
               color: "#FF0000", // Red
             },
             {
               title: "In - Amount",
-              value: "N/A",
+              value: sumOfTransactionTypeIn ?? "N/A",
               color: "#008000", // Green
             },
             {
               title: "Out - Amount",
-              value: "N/A",
+              value: sumOfTransactionTypeOut ?? "N/A",
               color: "#800000", // Maroon
             },
             {
               title: "In - Num",
-              value: "N/A",
+              value: numInTransactions ?? "N/A",
               color: "#00FF00", // Lime
             },
             {
               title: "Out - Num",
-              value: "N/A",
+              value: numOutTransactions ?? "N/A",
               color: "#808000", // Olive
             },
             {
